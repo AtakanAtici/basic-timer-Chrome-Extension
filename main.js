75d422108734a62fee3dda83b5b7810d582af467
Atakan ATICI <atakanatici0@gmail.com>
2021-05-03 01:01:58 +0300

    Final

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 var second = 0;
-		var minute = 0;
-		var hour   = 0;
-
+var minute = 0;
+var hour   = 0;
+var stext  = "s.";
+var mtext  = "m.";
+var htext  = "h.";
 
 	var interval = setInterval(function() {
 	    second++;
-	    $('.second').text(second);
+	    $('.second').text(second + stext);
 
 	    if (second === 60)
 	      {
 	        minute++;
-	        $('.minute').text(minute);
+	        $('.minute').text(minute + mtext);
 	        second = 0;
 	      }
 
 		  if (minute === 60)
 		    {
 		      hour++;
-		      $('.hour').text(hour);
+		      $('.hour').text(hour + htext);
 		      minute = 0;
 		    }
 	}, 1000);
